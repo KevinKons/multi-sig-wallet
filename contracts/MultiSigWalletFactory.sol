97848b4d29edc13b0b187c10aefd51d619b81d48
kevin.kons <kevin.kons@hotmail.com>
2022-06-10 15:53:03 -0300

    Refactoring MultiSigWalletFactory to be Ownable and receive/withdraw ethers

--- contracts/MultiSigWalletFactory.sol
+++ contracts/MultiSigWalletFactory.sol
@@ -2,17 +2,35 @@
 pragma solidity ^0.8.10;
 
 import './MultiSigWallet.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MultiSigWalletFactory {
+contract MultiSigWalletFactory is Ownable {
 
     event NewWallet(address);
+    event WithDraw(address destiny, uint value);
+    event Donation(address donor, uint value, bytes msg);
 
-    uint public whatever;
+    receive() external payable {
+        emit Donation(_msgSender(), msg.value, "");
+    }
 
-    function create(address[] calldata owners, uint required) external returns (address deployedAt) {
+    fallback() external payable {
+        emit Donation(_msgSender(), msg.value, _msgData());
+    }
+
+    function create(address[] calldata owners, uint required) external payable returns (address deployedAt) {
         MultiSigWallet multiSigWallet = new MultiSigWallet(owners, required);
         deployedAt = address(multiSigWallet);
         emit NewWallet(deployedAt);
-        whatever = required;
     }
+
+    function getBalance() external view returns (uint) {
+        return address(this).balance;
+    }
+
+    function withDraw() external onlyOwner {
+        emit WithDraw(_msgSender(), address(this).balance);
+        payable(_msgSender()).transfer(address(this).balance);
+    }
+    
 }

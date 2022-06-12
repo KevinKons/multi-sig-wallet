ebb22509070854412296f5bad7e243c8d6d2818f
kevin.kons <kevin.kons@hotmail.com>
2022-06-12 18:41:07 -0300

    Creating failure tests for MultiSigWallet

--- test/MultiSigWallet.test.ts
+++ test/MultiSigWallet.test.ts
@@ -96,7 +96,7 @@ describe('MultiSigWallet', () => {
     expect(await contract.approved(txId, owners[0].address)).to.be.false;
   });
 
-  it('When execute should should emit event and update balances', async () => {
+  it('When execute should emit event and update balances', async () => {
     const value = ethers.utils.parseEther('3');
     const tx = {
       to: contract.address,
@@ -126,4 +126,129 @@ describe('MultiSigWallet', () => {
     expect(await waffle.provider.getBalance(contract.address)).to.equal(contractPreviousBalance.sub(value));
     expect(await to.getBalance()).to.equal(toPreviousBalance.add(value));
   });
+
+
+  describe('Failure Tests', () => {
+    it('When deploying with empty owners array should revert with "Owners required"', async () => {
+      const MultiSigWalletFactory = await ethers.getContractFactory('MultiSigWallet');
+      await expect(MultiSigWalletFactory.deploy([], 2))
+        .to.be.revertedWith('Owners required')
+    });
+
+    it('When deploying if required is 0 should revert with "Invalid required numbers of owners"', async () => {
+      const MultiSigWalletFactory = await ethers.getContractFactory('MultiSigWallet');
+      await expect(MultiSigWalletFactory.deploy(owners.map(owner => owner.address), 0))
+        .to.be.revertedWith('Invalid required numbers of owners')
+    });
+
+    it('When deploying if required is bigger than owners.length should revert with "Invalid required numbers of owners"', async () => {
+      const MultiSigWalletFactory = await ethers.getContractFactory('MultiSigWallet');
+      await expect(MultiSigWalletFactory.deploy(owners.map(owner => owner.address), 4))
+        .to.be.revertedWith('Invalid required numbers of owners')
+    });
+
+    it('When deploying if owner is zero address should revert with "Invalid owner"', async () => {
+      const MultiSigWalletFactory = await ethers.getContractFactory('MultiSigWallet');
+      await expect(MultiSigWalletFactory.deploy(owners.map(owner => owner.address).concat(ethers.constants.AddressZero), 4))
+        .to.be.revertedWith('Invalid owner')
+    });
+
+    it('When deploying if owner is duplicated at owners array should revert with "Owner is already added"', async () => {
+      const MultiSigWalletFactory = await ethers.getContractFactory('MultiSigWallet');
+      await expect(MultiSigWalletFactory.deploy(owners.map(owner => owner.address).concat(owners[0].address), 4))
+        .to.be.revertedWith('Owner is already added')
+    });
+
+    it('When submiting if caller is not owner should revert with "Not owner"', async () => {  
+      const notOwner = (await ethers.getSigners())[numberOfOwners + 1];
+      const to = (await ethers.getSigners())[numberOfOwners].address;
+
+      await expect(contract.connect(notOwner).submit(to, 1, '0x'))
+        .to.be.revertedWith('Not owner');
+    });
+
+    it('When executing if approvals is not bigger than required should revert with "approvals < required"', async () => {  
+      const to = (await ethers.getSigners())[numberOfOwners];
+      await contract.submit(to.address, 1, '0x');
+
+      await expect(contract.execute(0))
+        .to.be.revertedWith('approvals < required');
+    });
+
+    it('When proposed transaction is not viable should revert with "Tx failed"', async () => {
+      const value = ethers.utils.parseEther('3');
+      const tx = {
+        to: contract.address,
+        value: value.sub(1)
+      }
+      await owners[1].sendTransaction(tx);
+  
+      const abi = [
+        "function anyFunc()"
+      ]
+      const abiInterface = new ethers.utils.Interface(abi);
+  
+      const to = (await ethers.getSigners())[numberOfOwners];
+      const data = abiInterface.encodeFunctionData("anyFunc");
+      await contract.submit(to.address, value, data);
+      const txId = 0;
+      await contract.approve(txId);
+      await contract.connect(owners[1]).approve(txId);
+      await contract.connect(owners[2]).approve(txId);
+  
+      await expect(contract.execute(txId))
+        .to.be.revertedWith('Tx failed');
+    });
+
+    it('When voting if proposal doesn\'t existed should revert with "Tx does not exists"', async () => {
+      await expect(contract.revoke(0))
+        .to.be.revertedWith('Tx does not exists')
+    });
+
+    it('When voting if proposal was already executed should revert with "Transaction already executed"', async () => {
+      const value = ethers.utils.parseEther('3');
+      const tx = {
+        to: contract.address,
+        value: value
+      }
+      await owners[1].sendTransaction(tx);
+
+      const abi = [
+        "function anyFunc()"
+      ]
+      const abiInterface = new ethers.utils.Interface(abi);
+
+      const to = (await ethers.getSigners())[numberOfOwners];
+      const data = abiInterface.encodeFunctionData("anyFunc");
+      const txId = 0;
+
+      await contract.submit(to.address, value, data);
+      await contract.approve(txId);
+      await contract.connect(owners[1]).approve(txId);
+      await contract.connect(owners[2]).approve(txId);
+      await contract.execute(txId);
+
+      await expect(contract.execute(txId))
+        .to.be.revertedWith('Transaction already executed');
+    });
+
+    it('When voting if proposal was already approved by owner should revert with "Tx already approved"', async () => {
+      const to = (await ethers.getSigners())[numberOfOwners];
+
+      await contract.submit(to.address, 1, '0x');
+      await contract.approve(0);
+
+      await expect(contract.approve(0))
+        .to.be.revertedWith('Tx already approved');
+    });
+
+    it('When revoking if proposal wasn\'t already approved by owner should revert with "Tx not approved"', async () => {
+      const to = (await ethers.getSigners())[numberOfOwners].address;
+      await contract.submit(to, 1, '0x');
+  
+      await expect(contract.revoke(0))
+        .to.be.revertedWith('Tx not approved')
+    });
+  });
 });
+

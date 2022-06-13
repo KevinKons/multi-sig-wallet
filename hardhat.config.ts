106b109b65edf2460d09dbabc32fb455d4845c58
kevin.kons <kevin.kons@hotmail.com>
2022-06-13 01:08:07 -0300

    Adding polygon networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,8 +11,13 @@ import 'hardhat-gas-reporter';
 dotenv.config();
 
 const ROPSTEN_URL: any = process.env.ROPSTEN_URL;
-const ACCOUNT_ONE: any = process.env.ACCOUNT_ONE;
 const ROPSTEN_ETHERSCAN_API_KEY: any = process.env.ROPSTEN_ETHERSCAN_API_KEY;
+
+const POLYGON_URL: any = process.env.POLYGON_URL;
+const POLYGON_MUMBAI_URL: any = process.env.POLYGON_MUMBAI_URL;
+const POLYGON_ETHERSCAN_API_KEY: any = process.env.POLYGON_ETHERSCAN_API_KEY;
+
+const ACCOUNT_ONE: any = process.env.ACCOUNT_ONE;
 const GAS_REPORTER_ENABLED: any = process.env.GAS_REPORTER_ENABLED;
 
 const config: HardhatUserConfig = {
@@ -21,11 +26,21 @@ const config: HardhatUserConfig = {
     ropsten: {
       url: ROPSTEN_URL,
       accounts: [ACCOUNT_ONE]
+    },
+    polygon: {
+      url: POLYGON_URL,
+      accounts: [ACCOUNT_ONE],
+    },
+    polygonMumbai: {
+      url: POLYGON_MUMBAI_URL,
+      accounts: [ACCOUNT_ONE],
     }
   },
   etherscan: {
     apiKey: {
-      ropsten: ROPSTEN_ETHERSCAN_API_KEY
+      ropsten: ROPSTEN_ETHERSCAN_API_KEY,
+      polygonMumbai: POLYGON_ETHERSCAN_API_KEY,
+      polygon: POLYGON_ETHERSCAN_API_KEY
     }
   },
   gasReporter: {
